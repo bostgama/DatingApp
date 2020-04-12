2e194440f17db5d23c3ac97b3550f8025c24f7f9
Garcia <JoseG@dep.nyc.gov>
2020-04-12 18:19:16 -0400

    second

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,3 +1,4 @@
+using DatingApp.API.Models;
 using DatingApp.API.Properties.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,6 @@ namespace DatingApp.API.Properties.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users {get; set;}
     }
 }
